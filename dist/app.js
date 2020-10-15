7a1fe762a4fa2077a3445682c1e6750cf2603211
Sen Cai <caisen97@gmail.com>
2020-10-15 12:31:22 -0400

    10/15/2020

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,18 @@
 var app = angular.module("myApp",[]);
 app.controller('myContr', function($scope, $http){
-    $http.get("/php/output.php")
+    $http.get("/php/messages_output.php")
     .then(function (response) {
         $scope.infos = response.data.Json_Data;
     });
+
+    $http.get("/php/controllers_output.php")
+    .then(function (response) {
+        $scope.controllers = response.data.Json_Data;
+    });
+
+    $scope.message_array = [];
+    $scope.addToArray = function(){
+
+    };
+
 });
--- php/getMessage.php
+++ php/getMessage.php
@@ -1,16 +1,29 @@
 <?php
 include 'sql.php';
 
-$query = mysqli_query($connection, "select * from messages");
-$data = array();
-$filename = "F:\Repo\SIRclient\php\output.php";
+$query_messages = mysqli_query($connection, "select * from messages");
+$data_messages = array();
+$filename_messages = "F:\Repo\SIRclient\php\messages_output.php";
 
-while ($row = mysqli_fetch_array($query))
+while ($row = mysqli_fetch_array($query_messages))
 {
-    $data[] = array("name"=>$row['name'],"type"=>$row['type'],"body"=>$row['body']);
+    $data_messages[] = array("name"=>$row['name'],"type"=>$row['type'],"body"=>$row['body']);
 }
 
 //echo json_encode($data);
-$data_Json = json_encode(['Json_Data' => $data]);
-file_put_contents($filename, $data_Json);
+$json_messages = json_encode(['Json_Data' => $data_messages]);
+file_put_contents($filename_messages, $json_messages);
+
+$query_controllers = mysqli_query($connection, "select CTRNAM, CTRDESCR from controllers");
+$data_controllers = array();
+$filename_controllers = "F:\Repo\SIRclient\php\controllers_output.php";
+
+while($row = mysqli_fetch_array($query_controllers))
+{
+    $data_controllers[] = array("ctrname" => $row['CTRNAM'],"describ" => $row['CTRDESCR']);
+}
+
+$json_controllers = json_encode(['Json_Data' => $data_controllers]);
+file_put_contents($filename_controllers,$json_controllers);
+
 ?>
